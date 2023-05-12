5ab39eabca15ae09335f8082122cd4b89978b0c8
luiz henrique <luizaaaljk2009@gmail.com>
2023-05-11 22:49:24 -0300

    projeto criado

---
+++ src/main/java/com/devsuperior/dslist3/Dslist3Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist3Application.class, args);
+	}
+
+}
